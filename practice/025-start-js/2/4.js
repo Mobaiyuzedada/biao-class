5f3c5d5dde306a0adbcc1ffd19bc31960deefd8b
xzdry <289331962@qq.com>
2018-11-15 12:48:22 +0800

    complete practice of function(038)

---
+++ practice/025-start-js/2/4.js
@@ -0,0 +1,20 @@
+;(function(){
+    'use strict';
+    console.log(6);
+})();
+;(function(){
+    'use strict';
+    console.log(7);
+})();
+;(function(){
+    'use strict';
+    console.log(8);
+})();
+;(function(){
+    'use strict';
+    console.log(9);
+})();
+;(function(){
+    'use strict';
+    console.log(10);
+})();
