d001d5ae4c46d5089390370277cf572cc82e19a5
xzdry <289331962@qq.com>
2019-02-08 20:54:33 +0800

    Complete the completed pagination plugin: display the specified number of pages, the rest are omitted.

--- second-practice/003-63-github用户搜索/js/main.js
+++ second-practice/003-63-github用户搜索/js/main.js
@@ -5,6 +5,7 @@
     let input = form.querySelector('[name=keyword]');
     let userList = document.querySelector('.user-list');
     let $ = rq;
+    let pageLimit = 7;//每页显示的用户数
 
     start();
 
@@ -17,32 +18,49 @@
         form.addEventListener('submit', e => {
             e.preventDefault();
             let keyWord = input.value;
-            render(keyWord);
+            search(keyWord);
         })
 
-        function render(keyWord) {
+        function search(keyWord, current = 1) {
             userList.innerHTML = '';
             if (!keyWord) {
                 alert('请输入搜索内容');
                 return;
             }
-            let url = `https://api.github.com/search/users?q=${keyWord}`;
+            let url = `https://api.github.com/search/users?q=${keyWord}&page=${current}&per_page=${pageLimit}`;
             $.get(url).then(data => {
-                let users = data.items;
-                users && users.forEach(user => {
-                    let item = document.createElement('div');
-                    item.classList.add('item');
-                    item.innerHTML = `
-                    <div class="avatar">
-                        <img src=${user.avatar_url} alt="用户头像">
-                    </div>
-                    <div class="detail">
-                        <strong><a target="_blank" href=${user.html_url}>${user.login}</a></strong>
-                        <div><a target="_blank" href=${user.html_url}>${user.html_url}</a></div>
-                    </div>
-                    `
-                    userList.appendChild(item);
+                //创建翻页组件
+                pagination.start({
+                    limit: pageLimit,
+                    amount: data.total_count > 1000 ? 1000 : data.total_count,
+                    el: '#pagination',
+                    current,
+                    onChange(page) {
+                        console.log(page);
+                        if (current == page)
+                            return;
+                        current = page;
+                        search(keyWord, current);
+                    }
                 })
+                render(data);
+            });
+        }
+        function render(data) {
+            let users = data.items;
+            users && users.forEach(user => {
+                let item = document.createElement('div');
+                item.classList.add('item');
+                item.innerHTML = `
+                <div class="avatar">
+                    <img src=${user.avatar_url} alt="用户头像">
+                </div>
+                <div class="detail">
+                    <strong><a target="_blank" href=${user.html_url}>${user.login}</a></strong>
+                    <div><a target="_blank" href=${user.html_url}>${user.html_url}</a></div>
+                </div>
+                `
+                userList.appendChild(item);
             })
         }
     }
--- second-practice/Plugin/002-pagination/lib/pagination.js
+++ second-practice/Plugin/002-pagination/lib/pagination.js
@@ -53,23 +53,59 @@
             <span class='shortcuts'><a href="#">&raquo</a></span>
             </div>
         `
+        insert.innerHTML = '';
         insert.appendChild(el);
         state.page.insert = el.querySelector('.primary-buttons');
         //缓存上一页、下一页
         state.page.shortcuts = el.querySelectorAll('.shortcuts');
     }
+
+    //生成页数
     function init(state) {
         let max = state.page.Max;//总页数
         let current = state.page.current = state.config.current;//当前页
         let insert = state.page.insert;//插入位置
-        let left = 1;//启始页
+        let length = state.page.length//显示的按钮个数
+        let left = Math.max(1, current - Math.floor(Number(length) / 2));//启始页
+
         insert.innerHTML = '';
-        let pages = new Array(max)
+        //根据left生成length个按钮，范围是[left,left+length]
+        let pages = new Array(length)
             .fill(0)
             .map((dummy, index) => index + left)
-            .filter(el => el > 0);
+            .filter(i => i <= max);
 
+        //当left+length>max的时候，即，pages里的个数少于length的时候，补全缺失的部分
+        if (pages.length < Number(length)) {
+            const padCount = Number(length) - pages.length;//计算缺少页数
+            const missing = pages[0] - padCount;//从多少页开始缺失的
+            let missingPages = new Array(padCount)
+                .fill(0)
+                .map((dummy, index) => missing + index)
+                .filter(el => el > 0);
+            pages = [...missingPages, ...pages];//完整的pages
+        }
+        //按要求显示按钮个数
+        //处理左侧省略
+        //pages[0]==left,即左端点为2时候,current已经到了中间:current=left+Math.floor(Number(length) / 2)
+        if (pages[0] && pages[0] > 1) {
+            pages.shift();//删除左侧一页
+            pages.unshift(null);//添加左侧省略页数标识
+            pages.unshift(1);//添加左侧首页
+        }
+        //处理右侧省略
+        if (pages[pages.length - 1] < Number(max)) {
+            pages.pop();
+            pages.push(null);
+            pages.push(Number(max));
+        }
         pages.forEach(page => {
+            if (!page) {
+                let span = document.createElement('span');
+                span.innerHTML = '...';
+                insert.appendChild(span);
+                return;
+            }
             let btn = document.createElement('a');
             btn.href = `#`;
             btn.innerHTML = page;
@@ -97,10 +133,8 @@
         })
     }
     function setCurrent(state, page) {
-        console.log(page);
         if (page < 1)
             return setCurrent(state, 1);
-
         if (page > state.page.Max)
             return setCurrent(state, state.page.Max);
         state.page.current = page;
@@ -109,5 +143,8 @@
             if (btn.$page == page)
                 btn.classList.add('active');
         })
+        // //返回给用户组件的状态和当前页
+        let onChange = state.config.onChange;
+        onChange && onChange(state.page.current, state);
     }
 })();
